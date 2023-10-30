c7bde37f3358777676e47e9ce5396079ff05edc9
igafarov90 <95170562+igafarov90@users.noreply.github.com>
2023-10-30 18:15:14 +0400

    Update Main.java

--- src/main/java/ru/it/tcinet/igafarov/Main.java
+++ src/main/java/ru/it/tcinet/igafarov/Main.java
@@ -2,6 +2,6 @@ package ru.it.tcinet.igafarov;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Igafarov!");
+        System.out.println("Hello, Ilgiz Gafarov!");
     }
 }
