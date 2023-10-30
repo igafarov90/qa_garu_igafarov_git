e75452c24a5ab851f61aa30076e1bf4afcf69687
igafarov <igafarov@dtln.ru>
2023-10-30 16:50:19 +0400

    hello message wsa changed

--- src/main/java/ru/it/tcinet/igafarov/Main.java
+++ src/main/java/ru/it/tcinet/igafarov/Main.java
@@ -2,6 +2,6 @@ package ru.it.tcinet.igafarov;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello igafarov!");
     }
 }
